2d4b4b8f36445fb45d7e2cde8af56dbe15b50974
Kishan Suthar <kishan.suthar@capsitech.com>
2024-07-19 10:39:35 +0530

    Declared variable to store sum,negatives numbers and regex to extract from string

--- stringCal.js
+++ stringCal.js
@@ -1,4 +1,20 @@
-const addString=(string)=>{
-  return string;
+const addString=(numbers)=>{
+
+    //if numbers or input string is empty then return 0 directly.
+    if (numbers === "") {
+        return 0;
+    }
+
+    // default delimiters: comma and newline
+    let delimiter = /,|\n/; 
+
+    // to check if the numbers string starts with a custom delimiter declaration. like // or newLine \n
+    let customDelimiterMatch = numbers.match(/^\/\/(.+)\n/);
+
+    let numArray = numbers.split(delimiter); // split the numbers in array by extracting delimiter pattern
+    let sum = 0;
+    let negatives = [];
+
+
 }
 console.log(addString("1,2,3"))

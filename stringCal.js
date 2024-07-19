c736ee26d5a2eb2bde884debf5ee37993cf4dbc8
Kishan Suthar <kishan.suthar@capsitech.com>
2024-07-19 11:04:09 +0530

    added replace delimiter according string

--- stringCal.js
+++ stringCal.js
@@ -11,6 +11,12 @@ const addString=(numbers)=>{
     // to check if the numbers string starts with a custom delimiter declaration. like // or newLine \n
     let customDelimiterMatch = numbers.match(/^\/\/(.+)\n/);
 
+    // if is any special char like // \n then replace new value/regex for delimiter
+    if (customDelimiterMatch) {
+        delimiter = new RegExp(customDelimiterMatch[1]);
+        numbers = numbers.substring(customDelimiterMatch[0].length);
+    }
+
     let numArray = numbers.split(delimiter); // split the numbers in array by extracting delimiter pattern
     let sum = 0;
     let negatives = [];
@@ -37,4 +43,15 @@ const addString=(numbers)=>{
     return sum;
 
 }
-console.log(addString("1,2,3"))
+
+// Test cases
+console.log(addString("")); // Output: 0
+console.log(addString("1")); // Output: 1
+console.log(addString("10,5")); // Output: 15
+console.log(addString("1\n2,3,7")); // Output: 13
+console.log(addString("//;\n1;2")); // Output: 3
+try {
+    console.log(addString("1,-2,3,-4")); // Should throw an exception
+} catch (e) {
+    console.error(e.message); // Output: negative numbers not allowed: -2, -4
+}
